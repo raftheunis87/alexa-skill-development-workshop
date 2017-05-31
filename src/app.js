6b1adf83b32348dac35ffc724664e69dd68b9839
raftheunis <raf.theunis@kbc.be>
2017-05-31 09:48:18 +0200

    Update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 const WELCOME_MESSAGE = 'Hello World!';
 
-module.exports.handlers = {
-    LaunchRequest() {
-        this.emitWithState('Start');
-    },
-    Start() {
-        this.emit(':tell', WELCOME_MESSAGE); 
+module.exports.newSessionHandlers = {
+    NewSession() {
+        this.emit(':tell', WELCOME_MESSAGE);
     }
 };
